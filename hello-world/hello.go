486b0879d3b267af8f56ab823aba2f91b43ce93f
Joel Thibault <thibault@broad.dev>
2023-11-29 17:26:03 -0500

    my hello world

---
+++ hello-world/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt" // A package in the Go standard library.
+)
+
+func main() {
+	// Println outputs a line to stdout.
+	// It comes from the package fmt.
+	fmt.Println("Hello world!")
+}
