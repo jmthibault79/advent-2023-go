c112f9be25122db790ed2e1cd00b7bf9fa31d469
Joel Thibault <thibault@broad.dev>
2023-12-03 16:18:44 -0500

    D3P1 done

--- src/advent/days/3.go
+++ src/advent/days/3.go
@@ -82,8 +82,60 @@ func parseLine(rowIdx int, line string) (s []Symbol, p []Part) {
 	return
 }
 
+func symbolAdjacent(part Part, symbolMap map[int]map[int]Symbol, lastRow, lastCol int) (adj bool, s Symbol) {
+	// define the search space: a box surrounding the part, clamped to the field edges
+	rowStart := max(part.row-1, 0)
+	rowEnd := min(part.row+1, lastRow)
+	colStart := max(part.start-1, 0)
+	colEnd := min(part.end+1, lastCol)
+
+	// man, for loops take me back
+	for row := rowStart; row <= rowEnd; row++ {
+		symbolMapRow := symbolMap[row]
+		if symbolMapRow != nil {
+			for col := colStart; col <= colEnd; col++ {
+				s := symbolMapRow[col]
+				if len(s.value) > 0 {
+					return true, s
+				}
+			}
+		}
+	}
+
+	return false, Symbol{}
+}
+
+func makeSymbolMap(symbols []Symbol) (out map[int]map[int]Symbol) {
+	out = make(map[int]map[int]Symbol)
+	for _, s := range symbols {
+		mapRow := out[s.row]
+		if mapRow == nil {
+			out[s.row] = make(map[int]Symbol)
+		}
+		out[s.row][s.pos] = s
+	}
+	return
+}
+
+func day3part1(symbols []Symbol, parts []Part, lastRow, lastCol int) int {
+	symbolMap := makeSymbolMap(symbols)
+	//fmt.Println(symbolMap)
+
+	adjacentPartsSum := 0
+
+	for _, p := range parts {
+		adj, s := symbolAdjacent(p, symbolMap, lastRow, lastCol)
+		if adj {
+			adjacentPartsSum += p.value
+			fmt.Println("Adjacent:", p, s)
+		}
+	}
+
+	return adjacentPartsSum
+}
+
 func main() {
-	inputFile := "../../inputs/test3.txt"
+	inputFile := "../../inputs/input3.txt"
 	lines := util.ReadLines(inputFile)
 
 	// Day 3 Part 1
@@ -116,4 +168,8 @@ func main() {
 	for _, p := range parts {
 		fmt.Println(p)
 	}
+
+	// for Part 1, which parts are adjacent to symbols?  add them.
+	result := day3part1(symbols, parts, len(lines)-1, len(lines[0])-1)
+	fmt.Println("Part1", result)
 }
