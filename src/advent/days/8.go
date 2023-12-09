7d7ef454b7e3f1843d2d30ebbd9b0d444f06dc05
Joel Thibault <thibault@broad.dev>
2023-12-08 20:05:56 -0500

    Day 8 Part 1

---
+++ src/advent/days/8.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"advent/util"
+	"fmt"
+	"strings"
+)
+
+const tooMany = 1_000_000
+const startNode = "AAA"
+const targetNode = "ZZZ"
+const leftChar = 'L'
+const leftBool = true
+
+type Node struct {
+	id    string
+	left  string
+	right string
+}
+type NodeMap map[string]Node
+
+// CCC = (ZZZ, GGG)
+func parseNode(line string) Node {
+	split1 := strings.Split(line, "=")
+	id := strings.TrimSpace(split1[0])
+
+	split2 := strings.Split(split1[1], ",")
+
+	left := strings.TrimSpace(strings.Replace(split2[0], "(", "", 1))
+	right := strings.TrimSpace(strings.Replace(split2[1], ")", "", 1))
+	return Node{id: id, left: left, right: right}
+}
+
+func parseInput(lines []string) (moves []bool, n NodeMap) {
+
+	for _, char := range lines[0] {
+		moves = append(moves, char == leftChar)
+	}
+
+	n = make(NodeMap)
+	for _, line := range lines[2:] {
+		node := parseNode(line)
+		n[node.id] = node
+	}
+	return moves, n
+}
+
+func day8part1(moves []bool, n NodeMap) (moveCount int) {
+	current := n[startNode]
+	fmt.Print(current.id)
+	for moveCount := 0; moveCount < tooMany; moveCount++ {
+		if current.id == targetNode {
+			fmt.Println()
+			return moveCount
+		}
+
+		if moves[moveCount%len(moves)] == leftBool {
+			current = n[current.left]
+		} else {
+			current = n[current.right]
+		}
+		fmt.Print(" -> ", current.id)
+	}
+	return tooMany
+}
+
+func main() {
+	lines := util.ReadInput("input", 8)
+	moves, nodeMap := parseInput(lines)
+	result := day8part1(moves, nodeMap)
+	fmt.Println("Part1", result)
+
+}
