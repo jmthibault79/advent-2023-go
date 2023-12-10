58d2042316a84d1d22af3f46702c403a8e6e53c2
Joel Thibault <thibault@broad.dev>
2023-12-10 16:54:43 -0500

    experimental performance optimization: GhostNodes are []string instead of []Node

--- src/advent/days/day8/part2.go
+++ src/advent/days/day8/part2.go
@@ -13,7 +13,7 @@ const targetRune = 'Z'
 const leftChar = 'L'
 
 // empirical values from input
-const numGhosts = 2
+const numGhosts = 6
 
 type Node struct {
 	id       string
@@ -21,41 +21,43 @@ type Node struct {
 	left     string
 	right    string
 }
-type GhostNode [numGhosts]Node
+type GhostNode [numGhosts]string
 type NodeMap map[string]Node
 
 func (gn GhostNode) String() string {
 	var b strings.Builder
-	b.WriteString(gn[0].id)
+	b.WriteString(gn[0])
 	for _, n := range gn[1:] {
-		b.WriteString("," + n.id)
+		b.WriteString("," + n)
 	}
 	return b.String()
 }
 
 func (gn GhostNode) left(m NodeMap) (out GhostNode) {
-	lefts := make([]Node, 0, numGhosts)
-	for _, n := range gn {
-		lefts = append(lefts, m[n.left])
+	lefts := make([]string, 0, numGhosts)
+	for _, nStr := range gn {
+		n := m[nStr]
+		lefts = append(lefts, m[n.left].id)
 	}
-	slices.SortFunc(lefts, nodeSliceComparator)
+	slices.Sort(lefts)
 	copy(out[:], lefts)
 	return
 }
 
 func (gn GhostNode) right(m NodeMap) (out GhostNode) {
-	rights := make([]Node, 0, numGhosts)
-	for _, n := range gn {
-		rights = append(rights, m[n.right])
+	rights := make([]string, 0, numGhosts)
+	for _, nStr := range gn {
+		n := m[nStr]
+		rights = append(rights, m[n.right].id)
 	}
-	slices.SortFunc(rights, nodeSliceComparator)
+	slices.Sort(rights)
 	copy(out[:], rights)
 	return
 }
 
-func (gn GhostNode) allTarget() bool {
+func (gn GhostNode) allTarget(m NodeMap) bool {
 	for idx := 0; idx < numGhosts; idx++ {
-		if gn[idx].endsWith != targetRune {
+		if m[gn[idx]].endsWith != targetRune {
 			return false
 		}
 	}
@@ -91,7 +93,7 @@ func nodeSliceComparator(a, b Node) int {
 	return cmp.Compare(a.id, b.id)
 }
 
-func addGhostNode(gp GhostPath, gn GhostNode, direction string) (out GhostPath) {
+func addGhostNode(gp GhostPath, gn GhostNode, direction string, m NodeMap) (out GhostPath) {
 	cycle, winner := false, false
 	// copy seen map - I think I was seeing same-reference problems otherwise
 	seen := make(map[GhostNode]int)
@@ -99,7 +101,7 @@ func addGhostNode(gp GhostPath, gn GhostNode, direction string) (out GhostPath) 
 		seen[k] = v
 	}
 
-	if gn.allTarget() {
+	if gn.allTarget(m) {
 		winner = true
 	} else if gp.seen[gn] > 0 {
 		cycle = true
@@ -109,7 +111,7 @@ func addGhostNode(gp GhostPath, gn GhostNode, direction string) (out GhostPath) 
 	return GhostPath{seen: seen, start: gp.start, end: gn, steps: gp.steps + direction, cycle: cycle, winner: winner}
 }
 
-func enumeratePaths(n NodeMap, start GhostNode) (cycles, winners []GhostPath) {
+func enumeratePaths(m NodeMap, start GhostNode) (cycles, winners []GhostPath) {
 	// init path queue with path at the start with 0 steps
 	initSeen := make(map[GhostNode]int)
 	initSeen[start] = 1
@@ -118,14 +120,24 @@ func enumeratePaths(n NodeMap, start GhostNode) (cycles, winners []GhostPath) {
 	// I don't want `range pathConsiderationQueue` here because I'm going to be expanding it
 	// so I want to check len() each time
 	for queueIdx := 0; queueIdx < len(pathConsiderationQueue); queueIdx++ {
+		if queueIdx%1_000_000 == 0 {
+			fmt.Println("idx ", queueIdx, " len ", len(pathConsiderationQueue))
+		}
 		currentPath := pathConsiderationQueue[queueIdx]
-		leftPath := addGhostNode(currentPath, currentPath.end.left(n), "L")
-		rightPath := addGhostNode(currentPath, currentPath.end.right(n), "R")
+		leftPath := addGhostNode(currentPath, currentPath.end.left(m), "L", m)
+		rightPath := addGhostNode(currentPath, currentPath.end.right(m), "R", m)
+
+		// attempt to help the garbage collector by removing something we don't need
+		pathConsiderationQueue[queueIdx] = GhostPath{}
 
 		for _, p := range []GhostPath{leftPath, rightPath} {
 			if p.winner {
+				fmt.Println("Winner", p)
 				winners = append(winners, p)
 			} else if p.cycle {
+				if len(cycles)%1000 == 0 {
+					fmt.Println("Cycles", len(cycles))
+				}
 				cycles = append(cycles, p)
 			} else {
 				// add to queue
@@ -137,42 +149,43 @@ func enumeratePaths(n NodeMap, start GhostNode) (cycles, winners []GhostPath) {
 	return
 }
 
-func parseInput(lines []string) (moves []bool, n NodeMap, start GhostNode) {
+func parseInput(lines []string) (moves []bool, m NodeMap, start GhostNode) {
 	for _, char := range lines[0] {
 		moves = append(moves, char == leftChar)
 	}
 
-	n = make(NodeMap)
-	startSlice := make([]Node, 0, numGhosts)
+	m = make(NodeMap)
+	startSlice := make([]string, 0, numGhosts)
 	for _, line := range lines[2:] {
 		node := parseNode(line)
 		if node.endsWith == startRune {
-			startSlice = append(startSlice, node)
+			startSlice = append(startSlice, node.id)
 		}
-		n[node.id] = node
+		m[node.id] = node
 	}
-	slices.SortFunc(startSlice, nodeSliceComparator)
+	slices.Sort(startSlice)
 	copy(start[:], startSlice)
-	return moves, n, start
+	return moves, m, start
 }
 
 func Part2(lines []string) int {
 	_, n, start := parseInput(lines)
 
 	// idea1: enumerate all of the possible outcomes (R, L, RR, RL, LR, LL, etc) and find the first that gets to all Z?
-	cycles, winners := enumeratePaths(n, start)
-	fmt.Println("Winners")
-	for _, p := range winners {
-		fmt.Println(p)
-	}
-	fmt.Println("Cycles")
-	for _, p := range cycles {
-		fmt.Println(p)
-	}
 
 	// idea2: find all paths from A^6 to Z^6, and all cycles from A^6 and Z^6 back to themselves
 
 	// idea3: include the history of "seen" nodes in a sequence so they can find cycles
+
+	cycles, winners := enumeratePaths(n, start)
+	fmt.Println("Winners (", len(winners), ")")
+	for _, p := range winners {
+		fmt.Println(p)
+	}
+	fmt.Println("Cycles (", len(cycles), ")")
+	//for _, p := range cycles {
+	//	fmt.Println(p)
+	//}
 
 	return 0
 }
