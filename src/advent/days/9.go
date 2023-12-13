43e2eccc55b99d4120540df84c304e5c63be6ba2
Joel Thibault <thibault@broad.dev>
2023-12-12 20:37:34 -0500

    D9P2 was not any harder

--- src/advent/days/9.go
+++ src/advent/days/9.go
@@ -8,8 +8,8 @@ import (
 // 1 2 3
 // 1 1
 // 0
-func nextVal(seq []int) (out int) {
-	diffs := make([][]int, 1)
+func getDiffs(seq []int) (diffs [][]int) {
+	diffs = make([][]int, 1)
 	diffs[0] = seq
 	for i := 0; !util.AllEqual(diffs[i], 0); i++ {
 		diffLen := len(diffs[i]) - 1
@@ -18,6 +18,11 @@ func nextVal(seq []int) (out int) {
 			diffs[i+1][j] = diffs[i][j+1] - diffs[i][j]
 		}
 	}
+	return
+}
+
+func nextVal(seq []int) (out int) {
+	diffs := getDiffs(seq)
 
 	// diffs has N rows.  diffs[N-1] is all-zero, so N-2 is the first useful one
 	// we want the sum of the last vals
@@ -28,9 +33,29 @@ func nextVal(seq []int) (out int) {
 	return
 }
 
+func prevVal(seq []int) (out int) {
+	diffs := getDiffs(seq)
+
+	// diffs has N rows.  diffs[N-1] is all-zero
+	// do this: curVal = 0.  for each diff [N-2 ... 0] curVal = diff[0] - curVal
+	for ii := len(diffs) - 2; ii >= 0; ii-- {
+		out = diffs[ii][0] - out
+	}
+	return
+}
+
 func day9part1(seqs [][]int) (out int) {
 	for _, seq := range seqs {
 		next := nextVal(seq)
+		out += next
+		fmt.Println(seq, " -> ", next)
+	}
+	return
+}
+
+func day9part2(seqs [][]int) (out int) {
+	for _, seq := range seqs {
+		next := prevVal(seq)
 		out += next
 		fmt.Println(seq, " -> ", next)
 	}
@@ -47,9 +72,11 @@ func parseInput9(lines []string) (out [][]int) {
 }
 
 func main() {
-	lines1 := util.ReadInput("input", "9")
-	sequences := parseInput9(lines1)
+	lines := util.ReadInput("input", "9")
+	sequences := parseInput9(lines)
 	result := day9part1(sequences)
 	fmt.Println("Part1", result)
 
+	result = day9part2(sequences)
+	fmt.Println("Part2", result)
 }
